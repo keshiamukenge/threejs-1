993f79d5c45f90daa371392e57cac027bc7a1dbf
keshiamukenge <keshia_mkg@live.fr>
2021-11-23 15:33:13 +0100

    modified shaders

--- src/assets/js/shaders/fragment.glsl
+++ src/assets/js/shaders/fragment.glsl
@@ -1,5 +1,17 @@
 varying float vNoise;
+varying vec2 vUv;
+uniform sampler2D imgTexture;
 
 void main() {
-    gl_FragColor = vec4(vNoise,0.,0.,1.);
+    vec3 color1 = vec3(1.,0.,0.);
+    vec3 color2 = vec3(1.,1.,1.);
+    vec3 finalColor = mix(color1,color2,0.5*(vNoise + 1.));
+
+    vec2 newUV = vUv;
+
+    vec4 imgView = texture2D(imgTexture,vUv);
+
+    gl_FragColor = vec4(finalColor,1.);
+    gl_FragColor = vec4(vUv,0.,1.);
+    /* gl_FragColor = imgView; */
 }
--- src/assets/js/shaders/vertex.glsl
+++ src/assets/js/shaders/vertex.glsl
@@ -75,16 +75,18 @@ float cnoise(vec3 P){
 
 uniform float time;
 varying float vNoise;
+varying vec2 vUv;
 
 void main() {
     vec3 newPosition = position;
     float PI = 3.1415925;
     /* newPosition.z += 0.1*sin((newPosition.x + 0.25 + time/8.)*2.*PI); */
     /* newPosition.z += 0.1*cnoise(vec3(position.x*4.,position.y*4. + time/4., 0.)); */
-    float noise = cnoise(vec3(position.x*4.,position.y*4. + time/5., 0.));
+    float noise = cnoise(vec3(position.x*4.,position.y*4. + time/5.,0.));
     newPosition.z += 0.1*noise;
 
     vNoise = noise;
+    vUv = uv;
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
 }
