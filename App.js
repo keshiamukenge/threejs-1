fce12ae16e98abe22a05b25d20e2b87d48e054f0
keshiamukenge <mukenge.keshia@gmail.com>
2021-11-24 00:21:10 +0100

    add animate technics for objects animations

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import GSAP from 'gsap';
 
 export default class App {
     constructor() {
@@ -6,73 +7,68 @@ export default class App {
         this.sizes = { width: 800, height: 600 }
 
         //Object
-        this.cube1 = new THREE.Mesh(
-            this.createGeometry(1, 1, 1),
-            this.createMaterial(0xff0000)
-        );
-
-        this.cube2 = new THREE.Mesh(
-            this.createGeometry(1, 1, 1),
-            this.createMaterial(0x00ff00)
-        );
-
-        this.cube3 = new THREE.Mesh(
-            this.createGeometry(1, 1, 1),
-            this.createMaterial(0x0000ff)
-        );
-        /* this.mesh = new THREE.Mesh(this.geometry, this.material); */
+        this.geometry = new THREE.BoxGeometry(1, 1, 1);
+        this.material = new THREE.MeshBasicMaterial({ color: 'red' });
+        this.cube = new THREE.Mesh(this.geometry, this.material);
         /* this.cubePos(); */
-        this.cube1.position.x = 1;
-        this.cube2.position.x = -1.5;
-        this.cube3.position.y = 1;
-
-        //Group
-        this.group = new THREE.Group();
-        console.log(this.cube1);
-        this.group.add(this.cube1, this.cube2, this.cube3);
-        this.group.scale.y = 2;
 
         //Camera
         this.camera = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height);
         this.cameraPos();
 
-        //Help tools
-        this.axesHelper = new THREE.AxesHelper(5);
+        //Time
+        this.clock = new THREE.Clock();
+
+        //GSAP
+        GSAP.to(this.cube.position, {
+            duration: 1,
+            delay: 1,
+            x: 2
+        });
+
+        GSAP.to(this.cube.position, {
+            duration: 1,
+            delay: 2,
+            x: 0
+        });
 
         //Scene
         this.scene = new THREE.Scene();
-        this.scene.add(this.cube1, this.camera, this.axesHelper, this.group);
+        this.scene.add(this.cube, this.camera);
 
         //Renderer
         this.renderer = new THREE.WebGLRenderer({
             canvas: this.canvas
         });
         this.rendererSize();
-        this.renderer.render(this.scene, this.camera);
+        
+        //Animate cube
+        this.animateCube();
     }
 
-    createGeometry(x, y, z) {
-        return this.geometry = new THREE.BoxGeometry(x, y, z); 
+    cubePos() {
+        this.cube.position.set(1, 1, 1);
     }
-
-    createMaterial(color) {
-        return this.material = new THREE.MeshBasicMaterial({ color: color });
-    }
-
-    /* cubePos() {
-        this.group.position.set(1, 1, 1);
-        this.cube1.scale.set(1, 1, 1);
-        this.cube1.rotation.reorder('XYZ');
-        this.cube1.rotation.set(Math.PI / 0.5, 1, Math.PI / 0.5);
-    } */
 
     cameraPos() {
-        this.camera.position.set(1, 1, 3);
-        this.camera.lookAt(this.cube1.position);
+        this.camera.position.z = 5;
     }
 
     rendererSize() {
         this.renderer.setSize(this.sizes.width, this.sizes.height);
+    }
+
+    animateCube() {
+        /* this.elapsedTime = this.clock.getElapsedTime(); */
+
+        /* this.camera.position.x = Math.sin(this.elapsedTime);
+        this.camera.position.y = Math.cos(this.elapsedTime);
+        this.camera.lookAt(this.cube.position); */
+        this.renderer.render(this.scene, this.camera)
+
+        window.requestAnimationFrame(() => {
+            this.animateCube();
+        });
     }
 }
 
