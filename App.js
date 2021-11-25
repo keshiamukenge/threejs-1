7905d69c552cc8b9b399dfdc55d247dc46013114
keshiamukenge <mukenge.keshia@gmail.com>
2021-11-25 16:07:33 +0100

    init basic shader

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import vertex from './src/assets/js/shaders/vertex.glsl';
+import fragment from './src/assets/js/shaders/fragment.glsl';
 import img from './src/assets/medias/img.jpeg';
 import arbre from './src/assets/medias/arbre.jpeg';
 import pierre from './src/assets/medias/pierre.jpeg';
@@ -21,54 +24,21 @@ export default class App {
         this.resizeScene();
 
         //MeshBasicMaterial
-        this.material = new THREE.MeshBasicMaterial();
-        this.material.map = this.arbreTexture;
-        /* this.material.color = new THREE.Color('red'); */
-        /* this.material.transparent = true;
-        this.material.opacity = 0.8; */
-        this.material.alphaMap = this.arbreTexture;
-        this.material.side = THREE.DoubleSide;
-        this.material.wireframe = false;
-
-        //MeshNormalMaterial
-        /* this.material = new THREE.MeshNormalMaterial();
-        this.material.map = this.arbreTexture;
-        this.material.side = THREE.DoubleSide;
-        this.material.wireframe = true;
-        this.material.flatShading = true; */
-
-        //MeshMatcapMaterial
-        /* this.material = new THREE.MeshMatcapMaterial();
-        this.material.matcap = this.arbreTexture;
-        this.material.side = THREE.DoubleSide; */
-
-        //MeshStandardMaterial
-        /* this.material = new THREE.MeshStandardMaterial(); */
-       /*  this.material.map = this.arbreTexture; */
-        /* this.material.side = THREE.DoubleSide;
-        this.material.metalness = 0.45;
-        this.material.roughness = 0.65; */
+        this.material = new THREE.RawShaderMaterial({
+            vertexShader: vertex,
+            fragmentShader: fragment
+        });
 
         //Object
-        this.sphere = new THREE.Mesh(
-            new THREE.SphereBufferGeometry(0.5, 16, 16),
-            this.material
-        );
-
         this.plane = new THREE.Mesh(
             new THREE.PlaneGeometry(1, 1, 12, 12, 13, 13),
-            this.material
-        );
-
-        this.torus = new THREE.Mesh(
-            new THREE.TorusBufferGeometry(0.3, 0.2, 16, 32),
             this.material
         );
 
         this.objectPos();
 
         //Camera
-        this.camera = new THREE.PerspectiveCamera(75, this.sizes.width / this.sizes.height, 0.1, 1000);
+        this.camera = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 1000);
         this.aspectRatio = this.sizes.width / this.sizes.height;
         this.cameraPos();
 
@@ -81,7 +51,7 @@ export default class App {
 
         //Scene
         this.scene = new THREE.Scene();
-        this.scene.add(this.sphere, this.plane, this.torus, this.camera);
+        this.scene.add(this.plane, this.camera);
 
         //Renderer
         this.renderer = new THREE.WebGLRenderer({
@@ -106,8 +76,8 @@ export default class App {
     }
 
     objectPos() {
-        this.sphere.position.x = - 1.5;
-        this.torus.position.x = 1.5;
+        this.plane.position.x = 2;
+        this.plane.position.y = 2;
     }
 
     enableDampingControl() {
@@ -127,12 +97,8 @@ export default class App {
         this.elapsedTime = this.clock.getElapsedTime();
         this.camera.lookAt(this.plane.position);
 
-        this.sphere.rotation.x = this.elapsedTime * Math.PI * 0.1;
-        this.sphere.rotation.y = this.elapsedTime * Math.PI * 0.1;
-        this.plane.rotation.x = this.elapsedTime * Math.PI * 0.1;
-        this.plane.rotation.y = this.elapsedTime * Math.PI * 0.1;
-        this.torus.rotation.x = this.elapsedTime * Math.PI * 0.1;
-        this.torus.rotation.y = this.elapsedTime * Math.PI * 0.1;
+        /* this.plane.rotation.x = this.elapsedTime * Math.PI * 0.1; */
+        /* this.plane.rotation.y = this.elapsedTime * Math.PI * 0.1; */
 
         /* this.controls.update(); */
 
